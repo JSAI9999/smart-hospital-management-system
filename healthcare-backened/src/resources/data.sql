1607b4842c2caed328fe910965ca1c7e89a4fd5a
JSAI9999 <jsaikumar99@gmail.com>
2026-08-07 12:05:07 +0530

    Add initial doctor data to data.sql

---
+++ healthcare-backened/src/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO doctors
+(doctor_name,specialization,qualification,phone,email,experience,consultation_fee)
+VALUES
+('Dr. John','Cardiology','MBBS','9876543210','john@gmail.com',10,500);
