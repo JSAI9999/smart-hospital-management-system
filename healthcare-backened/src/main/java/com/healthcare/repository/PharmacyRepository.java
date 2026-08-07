872f8f0f01dacd0bfe2d2f412288bce33660f054
JSAI9999 <jsaikumar99@gmail.com>
2026-08-07 10:36:44 +0530

    Create PharmacyRepository interface for JPA

---
+++ healthcare-backened/src/main/java/com/healthcare/repository/PharmacyRepository.java
@@ -0,0 +1,10 @@
+package com.healthcare.repository;
+
+import com.healthcare.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
